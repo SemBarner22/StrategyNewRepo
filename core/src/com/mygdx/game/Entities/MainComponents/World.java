31bdd8cce78ecec91173297a48d8f55e247b8c06
sembarner22 <ildar1001@mail.ru>
2020-02-07 21:54:52 +0300

    For Vova1

--- core/src/com/mygdx/game/Entities/MainComponents/World.java
+++ core/src/com/mygdx/game/Entities/MainComponents/World.java
@@ -30,7 +30,7 @@ public class World {
     public static ArrayList<Modificator> modificators = new ArrayList<>();
     public void initModGov() throws IOException{
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
-                new FileInputStream("res/CommonText/GovModificator")))) {
+                new FileInputStream("res/Inits/GovModificator")))) {
             String nextLine;
             bufferedReader.readLine();
             String[] init = new String[4];
